5bacd33d0b7ec1a99022853c2089fc20e85db0ab
brocode <kelimbth@gmail.com>
2023-03-24 21:22:02 +0300

    created a db table for tasks

--- api/models.py
+++ api/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 
+class Task(models.Model):
+    id = models.CharField(max_length=10, primary_key=True, unique=True, editable=False)
+    task = models.TextField(blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task[:5]
+
+    class Meta:
+        ordering = ['-created']
+
